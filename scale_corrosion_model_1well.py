406b2b076c9b4462f44b058230284dda501025ea
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-06-07 21:10:52 +0100

    Update scale_corrosion_model_1well.py

--- scale_corrosion_model_1well.py
+++ scale_corrosion_model_1well.py
@@ -838,7 +838,7 @@ def run():
                        
             st.write(results)
             
-            output1=str("%.2f" % df1.astype('float')) + ' mpy'
+            output1=str("%.2f" % float(df1)) + ' mpy'
             st.success('Velocidad de corrosion en cabeza: {}'.format(output1))
             
             #Perfil de la velocidad de corrosion
