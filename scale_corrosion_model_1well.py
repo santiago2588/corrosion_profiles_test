00443e915f7bb9c79f595a1a72eeb8969ebb1fa7
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-06-10 17:49:32 +0100

    Update scale_corrosion_model_1well.py

--- scale_corrosion_model_1well.py
+++ scale_corrosion_model_1well.py
@@ -900,6 +900,7 @@ def run():
             fig_sca.update_traces(mode="markers+lines")
             fig_sca.update_xaxes(showspikes=True, spikecolor='black')
             fig_sca.update_yaxes(showspikes=True,spikecolor='black')
+            fig_sca.update_yaxes(autorange="reversed")
             
             st.plotly_chart(fig_sca, use_container_width=True)
 
