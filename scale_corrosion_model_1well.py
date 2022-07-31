8093142a27b2105d2c909f97cb97e14ac74dd085
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-07-31 21:44:21 +0100

    Update scale_corrosion_model_1well.py

--- scale_corrosion_model_1well.py
+++ scale_corrosion_model_1well.py
@@ -1095,6 +1095,27 @@ def run():
             st.markdown("En esta ocasion, te hemos presentado los beneficios del modulo de Calculos. Sin embargo, quedan inmensas oportunidades para optimizar la productividad y el desempeño de los pozos petroleros al integrar todos los modulos de ASTRO") 
             st.markdown("Si estás listo para emprender este camino y llevar la digitalización al siguiente nivel, contactate con nosotros para guiarte en el proceso y recibir asesoria de nuestros expertos para optimizar tus operaciones.")
     
+            contact_form = """
+            <form action="https://formsubmit.co/zapaz.consultores@gmail.com" method="POST">
+                 <input type="hidden" name="_captcha" value="false">
+                 <input type="text" name="Nombre" placeholder="Tu nombre" required>
+                 <input type="email" name="email" placeholder="Tu email" required>
+                 <textarea name="message" placeholder="Tu mensaje"></textarea>
+                 <button type="submit">Send</button>
+            </form>
+            """
+
+            st.markdown(contact_form, unsafe_allow_html=True)
+
+            # Use Local CSS File
+            def local_css(file_name):
+                with open(file_name) as f:
+                    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+            local_css("style.css")
+
+    
     # Codigo para varios pozos
     
     if add_selectbox == 'Varios pozos':
