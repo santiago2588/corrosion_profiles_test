73be6bc25233d865e870d349e79e818660d91ed5
Santiago <santiago.zapata2588@gmail.com>
2022-09-10 15:34:46 +0100

    Added text

---
+++ pages/01_Informacion.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from PIL import Image
+
+st.markdown("# Informacion")
+
+with st.expander('Para que sirve ASTRO❓'):
+    
+    st.markdown("""
+    ### ASTRO es una herramienta digital para la gestion integral del tratamiento quimico en la industria petrolera""")
+    
+    st.markdown("ASTRO es una solucion modular que se adapta a tus necesidades y genera valor en tus operaciones. ASTRO incluye los siguientes modulos:")
+    
+    image1 = Image.open('Resources/modulos astro.png')
+    st.image(image1)
+      
+    st.markdown('En esta demostracion, te presentamos el Modulo de Calculos, que permite calcular la velocidad de corrosion, indice de saturacion y criticidad de los pozos petroleros.')
+                 
+with st.expander("Para que sirve el modulo de calculos de ASTRO❓"):
+
+    st.markdown("""### Este modulo permite predecir la velocidad de corrosión, índice de saturacion, y criticidad de los pozos de petróleo en función de
+         varios parametros de produccion y laboratorio. Para obtener las predicciones, selecciona la hoja Calculos. Luego, ajusta el
+         valor de cada parametro. Después de eso, da clic en el botón Calcular en la parte inferior para
+         ver las predicciones del modelo.""")
+
+    st.markdown(
+        """
+        El modulo de calculos tambien permite optimizar las dosis de los quimicos anticorrosivo y antiescala en funcion de los resultados
+            obtenidos en el modelo (riesgo de corrosion, incrustaciones y criticidad). Para ello, selecciona la hoja Optimizacion y da clic en el boton Optimizar en la parte inferior
+            y obtendras las dosis recomendadas de los quimicos, junto con el ahorro generado.
+                 """
+    )
