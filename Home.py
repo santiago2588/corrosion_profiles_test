ee7af068645d70546f5b3f68cc3be2e52f9906f2
Santiago <santiago.zapata2588@gmail.com>
2022-09-10 15:17:52 +0100

    Added text

--- Home.py
+++ Home.py
@@ -14,10 +14,10 @@ image = Image.open('Resources/logo_Pungo.png')
 st.image(image)
 
 st.write("# DIGITALIZACIÓN QUE GENERA IMPACTO")
-st.write("## Soluciones digitales para incrementar tus ingresos, disminuir tus costos operativos, reducir riesgos en tus operaciones y mejorar tu desempeño ambiental, desarrolladas a la medida de tus necesidades.")
+st.write("### Soluciones digitales para incrementar tus ingresos, disminuir tus costos operativos, reducir riesgos en tus operaciones y mejorar tu desempeño ambiental, desarrolladas a la medida de tus necesidades.")
 
 st.write("""
-        ## Bienvenido al demo de ASTRO: la herramienta digital para la gestión del tratamiento químico en la industria petrolera:
+        ### Bienvenido al demo de ASTRO: la herramienta digital para la gestión del tratamiento químico en la industria petrolera:
         - Reduce los costos asociados al tratamiento químico y las pérdidas de producción por eventos de corrosión e incrustaciones
         - Identifica los pozos críticos en tus operaciones con nuestra metodología para priorizar y optimizar el tratamiento químico
         - Disminuye el tiempo para el procesamiento de datos, genera reportes automáticos y libera tiempo valioso para optimizar la rentabilidad y seguridad de las operaciones
