d83366bd5ae88ab70428e57428478fa226039a79
Santiago <santiago.zapata2588@gmail.com>
2022-09-10 20:56:17 +0100

    Added text

--- Functions/funciones.py
+++ Functions/funciones.py
@@ -33,11 +33,6 @@
 
 import math
 import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import streamlit as st
-
 
 # Funcion para calcular ionic strength
 
