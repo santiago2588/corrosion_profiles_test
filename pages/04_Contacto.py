f530f05894d82febdf37bdb2b57c02074af63ecc
Santiago <santiago.zapata2588@gmail.com>
2022-09-18 14:36:33 +0100

    Added text

--- pages/04_Contacto.py
+++ pages/04_Contacto.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.markdown("## Felicitaciones, has comenzado a desbloquear algunos de los beneficios de la digitalización y el procesamiento de datos con modelos cientificos e inteligencia artificial.")
 
-st.markdown(""" ### En esta ocasion, te hemos presentado los beneficios del modulo de Calculos. Sin embargo, quedan inmensas oportunidades para optimizar la productividad y el desempeño de los pozos petroleros al integrar todos los modulos de ASTRO.
+st.markdown(""" En esta ocasion, te hemos presentado los beneficios del modulo de Calculos. Sin embargo, quedan inmensas oportunidades para optimizar la productividad y el desempeño de los pozos petroleros al integrar todos los modulos de ASTRO.
     
-    ### Si estás listo para emprender este camino y llevar la digitalización al siguiente nivel, contactate con nosotros para guiarte en el proceso y recibir asesoria de nuestros expertos para optimizar tus operaciones.
+    Si estás listo para emprender este camino y llevar la digitalización al siguiente nivel, contactate con nosotros para guiarte en el proceso y recibir asesoria de nuestros expertos para optimizar tus operaciones.
 """)
 
 contact_form = """
