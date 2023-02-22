55b9ce7c7e3827fada37b4f1c9a340fb13091bce
Santiago <santiago.zapata2588@gmail.com>
2023-02-22 16:39:43 +0000

    Added text

--- pages/02_Calculo_Individual.py
+++ pages/02_Calculo_Individual.py
@@ -453,14 +453,15 @@ def run():
 
     ahorro_total = ahorro_anual_ic + ahorro_anual_is
 
-    corr_button=st.expander('Resultados de corrosion')
+    #corr_button=st.button('Resultados de corrosion')
 
     # if "corr_state" not in st.session_state:
     #     st.session_state.corr_state = False
     #
     # if corr_button or st.session_state.corr_state:
     #     st.session_state.corr_state=True
-    col1, col2, col3 = st.columns(3)
+    with st.expander('Resultados de corrosion'):
+        col1, col2, col3 = st.columns(3)
 
         with col1:
             st.metric('Velocidad de corrosion en cabeza', str("%.2f" % np.float_(df1[0])) + ' MPY')
