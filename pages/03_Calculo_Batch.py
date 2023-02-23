1f371b0350e258afda6ff752d88de06a82a3a841
Santiago <santiago.zapata2588@gmail.com>
2023-02-23 18:23:42 +0000

    Added text

--- pages/03_Calculo_Batch.py
+++ pages/03_Calculo_Batch.py
@@ -514,7 +514,7 @@ def run():
                 fig_sca.update_yaxes(showspikes=True, spikecolor='black')
                 fig_sca.update_yaxes(autorange="reversed")
                 #st.plotly_chart(fig_sca, use_container_width=True)
-                plot_figures([fig_sca])
+                plot_figures([fig_sca],[i])
 
 
         with st.expander('Criticidad de pozos'):
