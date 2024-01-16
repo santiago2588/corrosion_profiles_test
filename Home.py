8017c55a39238dacace930ff85bea40d12e66073
Santiago <santiago.zapata2588@gmail.com>
2024-01-16 10:45:33 +0000

    d

--- Home.py
+++ Home.py
@@ -9,9 +9,9 @@ import streamlit as st
 
 st.set_page_config(layout="wide",page_title="ASTRO",page_icon="🌿")
 
-from PIL import Image
-image = Image.open('Resources/logo_Pungo.png')
-st.image(image)
+#from PIL import Image
+#image = Image.open('Resources/logo_Pungo.png')
+#st.image(image)
 
 st.write("# DIGITALIZACIÓN QUE GENERA IMPACTO")
 st.write("### Bienvenido al demo de ASTRO: la herramienta digital para la gestion del tratamiento quimico en la industria petrolera, desarrollada a la medida de tus necesidades.")
